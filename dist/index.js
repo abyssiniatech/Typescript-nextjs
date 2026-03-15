6fb0f85b84ca16bd48270ae155932053033cb298
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-15 20:03:48 +0300

    add primitive types

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// datatypes in typescript
+// 1. numbers 
+let num1 = 12;
+const num2 = 23;
+const num3 = 28;
+// run the values
+console.log(num1);
+console.log(num2);
+console.log(num3);
+// 2. string typescript
+const Firstname = "Surafel";
+const Lastname = "Berehun";
+const email = "surafelmengist@gmail.com";
+// run 
+console.log(Firstname);
+console.log(Lastname);
+console.log(email);
+// 3. boolean
+let isLogin = true;
+const isStudent = false;
+console.log(isLogin);
+console.log(isStudent);
+// 4 any type
+const fraind = 12;
+console.log(fraind);
+// 5 arry
+let numbers = [1, 2, 3, 4];
+let names = ["John", "Sara"];
+console.log(numbers.map((list) => console.log(list)));
+console.log(names);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+// datatypes in typescript
+// 1. numbers 
+let num1=12;
+const num2:number=23;
+const num3:number=28;
+
+// run the values
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+
+// 2. string typescript
+const Firstname:string="Surafel";
+const Lastname:string="Berehun";
+const email:string="surafelmengist@gmail.com"
+// run 
+console.log(Firstname)
+console.log(Lastname)
+console.log(email)
+
+
+// 3. boolean
+let isLogin:boolean=true;
+const isStudent:boolean=false;
+
+console.log(isLogin)
+console.log(isStudent)
+
+// 4 any type
+const fraind:any=12
+console.log(fraind)
+
+// 5 arry
+let numbers: number[] = [1,2,3,4]
+let names: string[] = ["John","Sara"]
+console.log(numbers.map((list)=>console.log(list)))
+console.log(names)
